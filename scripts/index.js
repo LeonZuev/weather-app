f679405b887d9aef91785173538e2e5f935e22c8
Leon Zuev <loonatune@gmail.com>
2023-05-25 20:26:30 +0300

    Add class solution

---
+++ scripts/index.js
@@ -0,0 +1,15 @@
+async function loadInfo(){
+  try {
+    const response = await fetch("https://get.geojs.io/v1/ip/geo.json");
+    const geoObj = await response.json();
+    const {city, latitude, longitude} = geoObj;
+  console.log(city, latitude, longitude);
+  }
+  catch(err){
+    console.log(err);
+
+  }
+  
+}
+
+loadInfo();
