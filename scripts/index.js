886bd48d9ce166ebeaa81bce1a05c0ac039b453b
Leon Zuev <loonatune@gmail.com>
2023-05-25 21:49:15 +0300

    Fix funktions

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,27 @@
-async function loadInfo(){
+async function loadInfo() {
   try {
     const response = await fetch("https://get.geojs.io/v1/ip/geo.json");
     const geoObj = await response.json();
-    const {city, latitude, longitude} = geoObj;
-  console.log(city, latitude, longitude);
-  const cityElement = document.querySelector(".cityGeo");
-  cityElement.textContent = city;
+    const { city, latitude, longitude } = geoObj;
+    console.log(city, latitude, longitude);
+    const cityElement = document.querySelector(".cityGeo");
+    cityElement.textContent = city;
+    loadWeatherData(latitude, longitude);
   }
-  catch(err){
+  catch (err) {
     console.log(err);
-
   }
-  
+}
+loadInfo();
+
+async function loadWeatherData(latitude, longitude) {
+  try {
+    const apiKey = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,rain,weathercode,winddirection_10m`;
+    const response = await fetch(apiKey);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-loadInfo();
