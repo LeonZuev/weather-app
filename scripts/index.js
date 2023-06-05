f73dcd4afe8f1463ca33e183bd7476fea70f1b19
Leon Zuev <loonatune@gmail.com>
2023-06-06 02:04:30 +0300

    add working solution

--- scripts/index.js
+++ scripts/index.js
@@ -8,7 +8,7 @@ async function loadInfo() {
     console.log(city, latitude, longitude);
     const cityElement = document.querySelector(".cityGeo");
     cityElement.textContent = city;
-    await loadWeatherData(latitude, longitude);
+    loadWeatherData(latitude, longitude);
   } catch (err) {
     console.log(err);
   }
@@ -21,21 +21,19 @@ async function loadWeatherData(latitude, longitude) {
     const data = await response.json();
     console.log(data);
 
-    const tempElement = document.querySelector(".temp");
-    const windElement = document.querySelector(".wind");
-    const weatherCodeElement = document.querySelector(".weatherCode");
-    const rainMmElement = document.querySelector(".rainMm");
+    // Поиск элементов лучше производить по ID
+    const tempElement = document.getElementById("temp");
+    const windElement = document.getElementById("wind");
+    const weatherCodeElement = document.getElementById("weatherCode");
+    // const rainMmElement = document.getElementById("rainMm");
+    console.log(data.current_weather);
+    // ДЕСТРУКТУРИЗАЦИЯ
+    const { temperature, windspeed, weathercode } = data.current_weather;
+    console.log(temperature, windspeed, weathercode);
 
-    if (data && data.hourly && data.hourly.length > 0) {
-      const hourlyData = data.hourly[0];
-
-      tempElement.textContent = hourlyData.temperature_2m_1h?.value + "°C";
-      windElement.textContent = hourlyData.winddirection_10m_1h?.value + "°";
-      weatherCodeElement.textContent = hourlyData.weathercode?.value;
-      rainMmElement.textContent = hourlyData.rain_1h?.value + " mm";
-    } else {
-      console.log("Недоступны данные о погоде");
-    }
+    tempElement.textContent = temperature + "°C";
+    windElement.textContent = windspeed + "°";
+    weatherCodeElement.textContent = weathercode;
   } catch (error) {
     console.log(error);
   }
