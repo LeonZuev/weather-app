50d37a6cc5ae694ed1da7889f459e37c17bcbd9d
Leon Zuev <loonatune@gmail.com>
2023-06-06 06:16:43 +0300

    Switch weathercode

--- scripts/index.js
+++ scripts/index.js
@@ -32,10 +32,58 @@ async function loadWeatherData(latitude, longitude) {
 
     tempElement.textContent = temperature + "°C";
     windElement.textContent = windspeed + " km/h";
-    weatherCodeElement.textContent = weathercode;
+
+    let weatherDescription;
+    switch (weathercode) {
+      case 0:
+        weatherDescription = "Clear sky";
+        break;
+      case 1:
+      case 2:
+      case 3:
+        weatherDescription = "Mainly clear, partly cloudy, and overcast";
+        break;
+      case 45:
+      case 48:
+        weatherDescription = "Fog and depositing rime fog";
+        break;
+      case 51:
+      case 53:
+      case 55:
+        weatherDescription = "Drizzle: Light, moderate, and dense intensity";
+        break;
+      case 56:
+      case 57:
+        weatherDescription = "Freezing Drizzle: Light and dense intensity";
+        break;
+      case 61:
+      case 63:
+      case 65:
+        weatherDescription = "Rain: Slight, moderate and heavy intensity";
+        break;
+      case 66:
+      case 67:
+        weatherDescription = "Freezing Rain: Light and heavy intensity";
+        break;
+      case 71:
+      case 73:
+      case 75:
+        weatherDescription = "Snow fall: Slight, moderate, and heavy intensity";
+        break;
+      case 77:
+        weatherDescription = "Snow grains";
+        break;
+      case 80:
+      case 81:
+      case 82:
+        weatherDescription = "Rain showers: Slight, moderate, and violent";
+        break;
+      default:
+        weatherDescription = "Unknown";
+    }
+    weatherCodeElement.textContent = weatherDescription;
   } catch (error) {
     console.log(error);
   }
 }
-
 loadInfo();
